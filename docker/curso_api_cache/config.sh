50c65ffeea6527d698d300a8a5632a498568485b
Alisson Couto <alissoncoutoads@outlook.com>
2021-12-27 00:27:41 -0300

    configs

--- docker/curso_api_cache/config.sh
+++ docker/curso_api_cache/config.sh
@@ -1,3 +1,4 @@
 docker-compose exec curso_api_cache composer install
 docker-compose exec curso_api_cache php artisan key:generate
+docker-compose exec curso_api_cache php artisan telescope:install
 docker-compose exec curso_api_cache php artisan migrate
